02f0e217b58dec8093df2c3945dcfd5a271351f0
Jinwoo Kim <0126kjw@kakao.com>
2022-10-20 17:01:22 +0900

    update OAuth.js

--- front/src/pages/login/OAuth.js
+++ front/src/pages/login/OAuth.js
@@ -1,4 +1,4 @@
 export const REST_API_KEY = "9901ca27a0f11eb036ff645bf90c0204";
-export const REDIRECT_URI =  "http://localhost:3000/auth/kakao/callback";
+export const REDIRECT_URI =  "https://veganner.netlify.app/auth/kakao/callback";
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
